88949ae433ba907f03a6982a22db2f90ad8f5e14
João Roberto de Oliveira Rêgo <joaocte@gmail.com>
2021-08-15 16:41:42 -0300

    criado use case para cadastro de pessoas.

---
+++ Jr.Backend.Pessoa.Application/UseCases/CadastrarPessoa/ICadastrarPessoaUseCase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jr.Backend.Pessoa.Application.UseCases.CadastrarPessoa
+{
+    public interface ICadastrarPessoaUseCase : IDisposable
+    {
+        Task ExecuteAsync();
+
+        void SetOutputPort(IOutputPort outputPort);
+    }
+}
---
+++ Jr.Backend.Pessoa.Application/UseCases/CadastrarPessoa/IOutputPort.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jr.Backend.Pessoa.Application.UseCases.CadastrarPessoa
+{
+    public interface IOutputPort
+    {
+        /// <summary>
+        ///     Invalid input.
+        /// </summary>
+        void Invalid();
+
+        /// <summary>
+        ///     Pessoa cadastrada com sucesso.
+        /// </summary>
+        void Ok(Domain.Pessoa pessoa);
+
+        /// <summary>
+        ///     Pessoa já cadastrada.
+        /// </summary>
+        void Conflict();
+    }
+}
