39fec34bb4434061b7342f7c2564ce64af1ab504
João Roberto de Oliveira Rêgo <joaocte@gmail.com>
2021-10-26 19:17:42 -0300

    removido parametros desnecessários visto que é um evento e não um comando.

--- Jr.Backend.Pessoa.Application/AutoMapper/MappingProfileToEnvent.cs
+++ Jr.Backend.Pessoa.Application/AutoMapper/MappingProfileToEnvent.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Jr.Backend.Message.Events.Pessoa;
+using Jr.Backend.Message.Events.Pessoa.Dto;
 
 namespace Jr.Backend.Pessoa.Application.AutoMapper
 {
@@ -8,14 +9,9 @@ namespace Jr.Backend.Pessoa.Application.AutoMapper
         public MappingProfileToEnvent()
         {
             CreateMap<Infrastructure.Entity.Endereco, Endereco>();
-
-            CreateMap<Infrastructure.Entity.Pessoa, PessoaCadastradaEvent>()
-                .ForMember(dest => dest.Enderecos, m => m.MapFrom(source => source.Enderecos))
-                .ForMember(dest => dest.Nome, m => m.MapFrom(source => source.NomeCompleto.Nome))
-                .ForMember(dest => dest.Sobrenome, m => m.MapFrom(source => source.NomeCompleto.Sobrenome))
-                .ForMember(dest => dest.Cpf, m => m.MapFrom(source => source.Documentos.Cpf))
-                .ForMember(dest => dest.Rg, m => m.MapFrom(source => source.Documentos.Rg))
-                .ForMember(dest => dest.TituloEleitoral, m => m.MapFrom(source => source.Documentos.TituloEleitoral)).ReverseMap();
+            CreateMap<Infrastructure.Entity.Documentos, Documentos>();
+            CreateMap<Infrastructure.Entity.NomeCompleto, NomeCompleto>();
+            CreateMap<Infrastructure.Entity.Pessoa, PessoaCadastradaEvent>();
         }
     }
 }
--- Jr.Backend.Pessoa.Application/UseCases/CadastrarPessoa/CadastrarPessoaUseCase.cs
+++ Jr.Backend.Pessoa.Application/UseCases/CadastrarPessoa/CadastrarPessoaUseCase.cs
@@ -7,8 +7,6 @@ using Jr.Backend.Pessoa.Domain.Commands.Responses;
 using Jr.Backend.Pessoa.Domain.Excepitons;
 using Jr.Backend.Pessoa.Infrastructure.Interfaces;
 using MassTransit;
-using Microsoft.Extensions.Configuration;
-using System;
 using System.Threading.Tasks;
 
 namespace Jr.Backend.Pessoa.Application.UseCases.CadastrarPessoa
@@ -21,15 +19,13 @@ namespace Jr.Backend.Pessoa.Application.UseCases.CadastrarPessoa
         private readonly IUnitOfWork unitOfWork;
         private readonly IMapper mapper;
         private readonly IBus bus;
-        private Uri uriQueue;
 
-        public CadastrarPessoaUseCase(IPessoaRepository pessoaRepository, IUnitOfWork unitOfWork, IMapper mapper, IBus bus, IConfiguration configuration)
+        public CadastrarPessoaUseCase(IPessoaRepository pessoaRepository, IUnitOfWork unitOfWork, IMapper mapper, IBus bus)
         {
             this.pessoaRepository = pessoaRepository;
             this.unitOfWork = unitOfWork;
             this.mapper = mapper;
             this.bus = bus;
-            uriQueue = new($"{configuration["RabbitSetting:UriBase"]}/{typeof(PessoaCadastradaEvent).Name}");
         }
 
         public async Task<CadastrarPessoaRespose> ExecuteAsync(CadastrarPessoaRequest cadastrarPessoaRequest)
@@ -43,15 +39,13 @@ namespace Jr.Backend.Pessoa.Application.UseCases.CadastrarPessoa
 
             var pessoaEntity = mapper.Map<Infrastructure.Entity.Pessoa>(pessoa);
 
-            PessoaCadastradaEvent @event = mapper.Map<PessoaCadastradaEvent>(pessoaEntity);
-
             await pessoaRepository.AddAsync(pessoaEntity);
 
             await unitOfWork.CommitAsync();
 
-            var endpoint = await bus.GetSendEndpoint(uriQueue);
+            PessoaCadastradaEvent @event = mapper.Map<PessoaCadastradaEvent>(pessoaEntity);
 
-            await endpoint.Send(@event);
+            await bus.Publish(@event);
             return new CadastrarPessoaRespose
             {
                 Id = pessoaEntity.Id.ToGuid()
