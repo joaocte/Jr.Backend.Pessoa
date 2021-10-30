186f1f3cd5d8cb753a0025c2fcd215e24a504978
João Roberto de Oliveira Rêgo <joaocte@gmail.com>
2021-10-30 12:19:39 -0300

    ajustes na api.

--- Jr.Backend.Pessoa.Api/Controllers/V1/PessoaController.cs
+++ Jr.Backend.Pessoa.Api/Controllers/V1/PessoaController.cs
@@ -1,6 +1,7 @@
 ﻿using Jr.Backend.Pessoa.Domain.Commands.Requests;
 using Jr.Backend.Pessoa.Domain.Commands.Responses;
 using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Threading.Tasks;
@@ -16,35 +17,38 @@ namespace Jr.Backend.Pessoa.Api.Controllers.V1
     {
         // GET api/<PessoaController>/5
         [HttpGet("{id}")]
-        public Task<ObterPessoaPorIdResponse> Get([FromServices] IMediator mediator, Guid id)
+        public async Task<ObterPessoaPorIdResponse> Get([FromServices] IMediator mediator, Guid id)
         {
-            return mediator.Send(new ObterPessoaPorIdRequest(id));
+            return await mediator.Send(new ObterPessoaPorIdRequest(id));
         }
 
         // POST api/<PessoaController>
         [HttpPost]
-        public Task<CadastrarPessoaRespose> Post([FromServices] IMediator mediator,
+        public async Task<CadastrarPessoaRespose> Post([FromServices] IMediator mediator,
             [FromBody] CadastrarPessoaRequest command)
         {
-            return mediator.Send(command);
+            return await mediator.Send(command);
         }
 
         // PUT api/<PessoaController>/5
         [HttpPut("{id}")]
-        public Task<AtualizarPessoaResponse> Put(Guid id, [FromBody] PessoaResquest request, [FromServices] IMediator mediator)
+        public async Task<AtualizarPessoaResponse> Put(Guid id, [FromBody] PessoaResquest request, [FromServices] IMediator mediator)
         {
             AtualizarPessoaRequest command = new()
             { Documentos = request.Documentos, Enderecos = request.Enderecos, Id = id, NomeCompleto = request.NomeCompleto };
 
-            return mediator.Send(command);
+            return await mediator.Send(command);
         }
 
         // DELETE api/<PessoaController>/5
         [HttpDelete("{id}")]
-        public Task Delete(Guid id, [FromServices] IMediator mediator)
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Delete(Guid id, [FromServices] IMediator mediator)
         {
             var command = new DeletarPessoaRequest(id);
-            return Task.FromResult(mediator.Send(command));
+            await mediator.Send(command);
+            return NoContent();
         }
     }
 }
